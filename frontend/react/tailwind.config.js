c303d647e05428033de438057e4a712dd8010471
João Victor <joaocrivoi13@gmail.com>
2024-08-07 23:26:09 -0300

    Applyng Guests Modal and Emails add

--- frontend/react/tailwind.config.js
+++ frontend/react/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans: 'Inter',
+      },
+      boxShadow:{
+        shape: '8px 8px 8px rgba(0, 0, 0, 0.8), /* Dark shadow */8px 4px 4px rgba(0, 0, 0, 0.6), /* Medium shadow */8px 2px 2px rgba(0, 0, 0, 0.4), /* Light shadow */8px 8px 8px 1px rgba(0, 0, 0, 0.8), /* Dark shadow with spread */inset 8px 8px 8px 1px rgba(50, 50, 50, 0.5), /* Inset shadow with gray */inset 8px 1px 8px rgba(50, 50, 50, 0.3);'
+
+      },
+      backgroundImage:{
+        pattern: 'url(/bg.png)'
+      },
+    },
   },
   plugins: [],
 }
